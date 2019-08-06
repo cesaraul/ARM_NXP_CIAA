024470d54b65219a2db768d96c27474f0b55f231
cesar cruz <cesaraulcg@gmail.com>
2019-08-06 15:45:23 -0500

    first post

---
+++ project.mk
@@ -0,0 +1,25 @@
+#==============================================================================
+# Select a project to compile, or comment and uncomment an example
+#==============================================================================
+
+# ---------- Example of path inside this file folder -------------------
+# Project path
+#(relative to this folder, leave void if the project is in this folder)
+#PROJECT_PATH = examples_c/sapi/gpio
+# Project name
+#PROJECT_NAME = blinky
+
+# ---------- Example of path outside this file folder ------------------
+# Project path
+#PROJECT_PATH = ../examples
+# Project name
+#PROJECT_NAME = blinky
+
+# ---------- Your paths: -----------------------------------------------
+
+# Project path
+#PROJECT_PATH = examples_c/sapi/gpio
+#PROJECT_NAME = blinky_switch
+
+PROJECT_PATH = examples_c/sapi/rtos_freertos/static_mem
+PROJECT_NAME = freeRTOS_01_blinky
